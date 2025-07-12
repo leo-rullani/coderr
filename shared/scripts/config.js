5f1ae162cc0b49ba9c7debde8f131028540ec2e6
Leugzim Rullani <leugzimrullani@outlook.com>
2025-07-12 18:20:45 +0200

    Initial commit – Coderr Frontend

--- shared/scripts/config.js
+++ shared/scripts/config.js
@@ -1,39 +1,39 @@
 const GUEST_LOGINS = {
-    customer : {
+    customer: {
         username: 'andrey',
         password: 'asdasd'
     },
-    business : {
+    business: {
         username: 'kevin',
         password: 'asdasd24'
     }
-}
+};
 
 const API_BASE_URL = 'http://127.0.0.1:8000/api/';
 const STATIC_BASE_URL = 'http://127.0.0.1:8000/';
 
-
+// Auth endpoints
 const LOGIN_URL = 'login/';
-
 const REGISTER_URL = 'registration/';
 
-const PROFILE_URL = 'profile/';
-
+// User profile endpoints
+const PROFILE_URL = 'profile/';  // profile/<id>/
 const BUSINESS_PROFILES_URL = 'profiles/business/';
-
 const CUSTOMER_PROFILES_URL = 'profiles/customer/';
 
-const REVIEW_URL = 'reviews/';
-
-const ORDER_URL = 'orders/';
-
+// Offers endpoints
 const OFFER_URL = 'offers/';
-
 const OFFER_DETAIL_URL = 'offerdetails/';
 
-const BASE_INFO_URL = 'base-info/';
-
+// Orders endpoints
+const ORDER_URL = 'orders/';
 const OFFER_INPROGRESS_COUNT_URL = 'order-count/';
 const OFFER_COMPLETED_COUNT_URL = 'completed-order-count/';
 
-const PAGE_SIZE = 6
+// Reviews endpoint
+const REVIEW_URL = 'reviews/';
+
+// Core info endpoint
+const BASE_INFO_URL = 'base-info/';
+
+const PAGE_SIZE = 6;
